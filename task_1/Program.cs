80d2c6a1dff572edc0b04630783999d0989b9d4d
Алина Манагадзе <agmanagadze_1@edu.hse.ru>
2019-06-23 23:24:02 +0500

    Добавьте файлы проекта.

---
+++ task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n, k, m;
+            string s;
+            string[] split;
+            s = Console.ReadLine();
+            split = s.Split(new char[] { ' ' });
+            long.TryParse(split[0], out k);
+            for (int i = 0; i < k; i++)
+            {
+                s = Console.ReadLine();
+                split = s.Split(new char[] { ' ' });
+                long.TryParse(split[0], out n);
+                long.TryParse(split[1], out m);
+                if (n == 1) Console.WriteLine("No");
+                else if (((n - 1) * (n - 2)) >= (2 * m)) Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+            }
+        }
+    }
+}
